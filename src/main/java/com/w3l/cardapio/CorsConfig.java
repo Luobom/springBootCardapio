98dfc0a9b922bf68695c23f2e3841b705c43a3a7
Luciano Nascimento <luciano.m.nasc@gmail.com>
2023-09-26 11:58:18 -0300

    add CorsConfig.java

---
+++ src/main/java/com/w3l/cardapio/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.w3l.cardapio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/automations/**") // Ajuste o mapeamento conforme necessário
+                        .allowedOrigins("http://localhost:5173") // Permita a origem do seu aplicativo React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
